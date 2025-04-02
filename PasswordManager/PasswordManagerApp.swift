6aa56cfe12f081033510f956efc580987173b282
Ratna Verma <ratnav@chetu.com>
2025-04-02 16:14:32 +0530

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by chetu on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
